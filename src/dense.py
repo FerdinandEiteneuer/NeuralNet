fix activation fcts, backprop, add train_network.py training on mnist

src/dense.py
@@ -3,56 +3,88 @@ from functools import wraps
 from layer import Layer
 from kernel_initializer import kernels
 
+def check_nan(func):
+    check = ['w', 'b']
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func
+    return wrapper
 
 class Dense(Layer):
-    def __init__(self, input_dim, output_dim, activation, kernel_init, verbose=False):
+    def __init__(
+            self,
+            input_dim,
+            output_dim,
+            activation,
+            kernel_init,
+            kernel_regularizer=None,
+            bias_regularizer=None,
+            verbose=False):
+
+        super().__init__()
 
         self.verbose = verbose
         self.g = activation
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
         self.shape = output_dim, input_dim
 
         self.w = kernels[kernel_init](self.shape)
         self.b = np.zeros((output_dim, 1))
 
-        self.layer_id = next(self.__class__._ids)  # layer number automatically generated.
-        if self.verbose: print('AFTER INIT LAYER SHAPE:', self.w.shape)
+        self.dw = np.zeros(self.w.shape)
+        self.db = np.zeros(self.b.shape)
 
-    #def __call__(self, a):
-    #    return self.forward(a, gradient_check = False, grad_check_info=0)
 
     def __str__(self):
-        representation = f'Layer number {self.layer_id}. '\
+        representation = f'layer {self.layer_id} (fully connected) '\
                          f'w.shape={self.w.shape}, b.shape={self.b.shape}'
 
         return representation
 
-    def forward(self, a, gradient_check = False, grad_check_info=0):
-        if gradient_check:
-            w_temp = np.copy(self.w)
-            j, k, _, _, eps = grad_check_info
-            self.w[j,k] += eps
+    def forward(self, a):
 
         self.z = np.dot(self.w, a) + self.b
         self.a = self.g(self.z)
-
-        if gradient_check:
-            self.w = w_temp
-
         return self.a
+
+    def backward_step(self, a_next, w_prev, error_prev):
+
+        batch_size = a_next.shape[-1]
+
+        derivative = self.g(self.z, derivative=True)
+        error = np.dot(w_prev.T, error_prev) * derivative
+
+        self.dw = 1 / batch_size * np.dot(error, a_next.T)
+        self.db = 1 / batch_size * np.sum(error, axis=1, keepdims=True)
+        return error
 
     def get_grads(self, a_prev):
 
         N = self.error.shape[1]
-        if self.verbose: print(f'{N=}, inside get_grads, err.shape={self.error.shape}, a_prev.T.shape={a_prev.T.shape}')
+        if self.verbose: print(f'{N=}, inside get_grads, error.shape={self.error.shape}, a_prev.T.shape={a_prev.T.shape}')
         self.dw = np.dot(self.error, a_prev.T) / N
         self.db = np.sum(self.error, axis=1, keepdims=True) / N
+
+        try:
+            if np.any(np.isnan(self.dw)):
+                print('ERROR: NAN IN self.dw')
+            if np.any(np.isnan(self.db)):
+                print('ERROR: NAN IN self.db')
+        except TypeError as e:
+            print('bad type?')
+
         return self.dw, self.db
 
-    def get_error(self, back_err):
+    def get_error(self, back_err, verbose=True):
         derivative = self.g(self.z, derivative=True)
-        if self.verbose: print(f'in get_error {self.layer_id}, {back_err.shape}, {derivative.shape}, {self.z.shape}, {self.g}')
-        if self.verbose: print(self)
+        if verbose: print(f'in get_error {self.layer_id}, {back_err.shape}, {derivative.shape}, {self.z.shape}, {self.g}')
+        if verbose: print(f'{derivative=}')
+        if verbose: print(f'received {back_err=}')
+        if verbose: print(self)
         self.error = back_err * derivative
+        if verbose: print(f'{self.error=}')
+        #raise ValueError('hat es einen einfluss, was self.error, derivative sind?. Probier auch mal aus die get_error, backward funktionen zu vereinigen. baue einfach das delta und gut ist')
         return self.error
 
     def backward(self):
@@ -63,3 +95,4 @@ class Dense(Layer):
         if self.verbose: print(f'{self.layer_id}: {self.w.shape=}, {self.dw.shape=}\n   {self.b.shape=}, {self.db.shape=}')
         self.w -= lr * self.dw
         self.b -= lr * self.db
+

src/kernel_initializer.py
@@ -12,7 +12,7 @@ def glorot_uniform(shape):
 def normal(shape):
     w = 0.01 * np.random.normal(size=shape)
     return w
-   
+
 def xavier(shape):
     return None
 

src/loss_functions.py
@@ -2,25 +2,116 @@ import numpy as np
 from functools import wraps
 import sys
 
-def check_dims(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        ypred_dim = args[0].shape[1]
-        ytrue_dim = args[1].shape[1]
-        assert ypred_dim == ytrue_dim
-        return func(*args, **kwargs)
+class Lossfunction():
+    def __call__(self, ypred, ytrue, average_examples=None, derivative=False):
+        assert ypred.shape == ytrue.shape, f'function inputs are not equal: {ypred.shape=}, {ytrue.shape=}'
+        if derivative:
+            if average_examples is None:
+                average_examples = False
+            return self.derivative(ypred, ytrue, average_examples)
+        else:
+            if average_examples is None:
+                average_examples = True
+            return self.function(ypred, ytrue, average_examples)
+
+class _crossentropy(Lossfunction):
+
+    @staticmethod
+    def function(ypred, ytrue, average_examples=True):
+        if average_examples:
+            N = ytrue.shape[1]
+            loss = - 1 / N * np.sum(ytrue*np.log(ypred))
+            return loss
+        else:
+            loss_per_example = -np.sum(ytrue * np.log(ypred), axis=0, keepdims=True)
+            return loss_per_example
+
+    @staticmethod
+    def derivative(ypred, ytrue, average_examples=False):
+        if average_examples:
+            N = ypred.shape[1]  # num datapoints
+            der_loss = -1 / N * np.sum(ytrue / ypred)
+            return der_loss
+        else:
+            der_loss = - ytrue / ypred
+            return der_loss
+
+def crossentropy(ypred, ytrue, average_examples=True, derivative=False):
+
+    if derivative:
+        return _crossentropy.derivative(ypred, ytrue, average_examples=average_examples)
+    else:
+        return _crossentropy.function(ypred, ytrue, average_examples=average_examples)
+
+
+class mse_(Lossfunction):
+
+    @staticmethod
+    def function(ypred, ytrue, average_examples=True):
+        diff = ypred - ytrue
+        loss_per_example = np.sum(diff**2, axis=0, keepdims=True)
+        nb_examples = ytrue.shape[1]
+        if average_examples:
+            total_loss = np.sum(loss_per_example)/nb_examples
+            assert total_loss.shape == ()
+            return total_loss
+        else:
+            assert loss_per_example.shape == (1, nb_examples)
+            return loss_per_example
+
+    @staticmethod
+    def derivative(ypred, ytrue, average_examples=False):
+        if not average_examples:
+            derivative = 2 * (ypred - ytrue)
+            return derivative
+        else:
+            raise NotImplementedError
+
+def check_dims(lossfct):
+    """
+    Checks the dimensions of the input and outputs to the lossfunction.
+    """
+    @wraps(lossfct)
+    def wrapper(ypred, ytrue, derivative=False, average_examples=True):
+        loss = lossfct(ypred, ytrue, average_examples, derivative)
+        if average_examples:
+            assert loss.shape == (), f'dimensions of loss ({loss.shape} is false. should be (), i.e just a number'
+        else:
+            assert loss.shape == (1, ytrue_dim), f'dimensions of loss is false, should be (1, {ytrue_dim}), i.e the loss per example'
+        return loss
     return wrapper
-'''
-input lossfunction:
-    dim ypred=(output_dim, number trainingexamples)
-    dim ytrue=(output_dim, number trainingexamples)
-output lossfunction:
-    dim 
 
 
-input derivative_loss function:
-    dim ypred=(
-'''
+def mean_squared_error(ypred, ytrue, derivative=False, average_examples=True):
+    '''
+    Mean Squared Error.
+    If derivative is False, the function optionally computes the complete average loss.
+    If derivative is True, the function computes the matrix with idx (class, train_example).
+    '''
+    ypred_dim = ypred.shape[1]
+    ytrue_dim = ytrue.shape[1]
+    assert ypred_dim == ytrue_dim, f'dimensions of ytrue ({ytrue.shape}) and ypred ({ypred.shape}) don\'t match'
+
+    diff = ypred - ytrue
+
+    if not derivative:
+        loss_per_example = np.sum(diff**2, axis=0, keepdims=True)
+
+        if average_examples:
+            nb_examples = ypred.shape[1]
+            total_loss = np.sum(loss_per_example) / nb_examples
+            assert total_loss.shape == (), f'dimensions of loss ({loss.shape} is false. should be (), i.e just a number'
+            return total_loss
+        else:
+            assert loss_per_example.shape == (1, ytrue_dim), f'dimensions of loss is false, should be (1, {ytrue_dim}), i.e the loss per example'
+            return loss_per_example
+
+    else:
+        loss_per_example_and_class = 2 * diff
+        if not average_examples:
+            return loss_per_example_and_class
+        else:
+            raise NotImplementedError
 
 @check_dims
 def _mse(ypred, ytrue, verbose=False):
@@ -46,22 +137,3 @@ def mae(ypred, ytrue):
     N = ypred.shape[1]
     ratio = np.abs((ypred - ytrue) / ytrue)
     return ratio[0], float(np.squeeze(np.mean(ratio, axis=1, keepdims=True)))
-mean_average_error = mae
-
-def _cross_entropy(ypred, ytrue):
-    assert(ypred.shape == ytrue.shape)
-    N = ytrue.shape[1]
-    return - np.sum(ytrue*np.log(ypred), keepdims=True)
-def _derivative_cross_entropy(ypred, ytrue):
-    derivative = - np.sum(ytrue / ypred)
-    return derivative
-cross_entropy = {'function': _cross_entropy, 'derivative': _derivative_cross_entropy}
-    
-
-
-def derivative(ypred, ytrue, func='mse', arg=''):
-    func = 'derivative_' + func
-    if arg == '':
-        return eval(func)(ypred, ytrue)
-    else:
-        return eval(func)(ypred, ytrue, arg)

src/network.py
@@ -1,6 +1,12 @@
-import layer
-from flatten import Flatten
 import numpy as np
+
+from functools import partial
+import itertools
+import warnings
+
+import layer
+from activations import softmax
+import misc
 
 class Network(list):
 
@@ -11,20 +17,22 @@ class Network(list):
         self.verbose=verbose
 
         self.append(layer.Layer())  # starting layer, for holding input values to the network
+        self.softmax = None  # True if last act fct is softmax
 
     def __str__(self):
+        s = 17*'*' + '\nmodel information:\n'
+
         number_params = 0
         params = ['w','b','beta','gamma']
         for layer in self:
+            s += str(layer) + '\n'
             for param in params:
                 if hasattr(layer,param):
                     N = np.multiply.accumulate(getattr(layer,param).shape)[-1]
                     number_params += N
 
-        s = 'model information:\n'
-        s += '  layers: %i\n' % len(self)
         s += '  number of parameters: %i' % number_params
-        print(s)
+        return s
 
     def __call__(self, a):
         return self.forward_step(a)
@@ -32,7 +40,7 @@ class Network(list):
     def add(self, layer):
         self.append(layer)
 
-    def compile(self, loss, lr):
+    def compile(self, loss, lr, optimizer=None):
         self.batch_norm = False
         for l in self.layers:
             try:
@@ -42,104 +50,209 @@ class Network(list):
                 pass
 
         self.lr = lr
-        self.lossfunction = loss['function']
-        self.derivative_lossfunction = loss['derivative']
+        self.loss_fct = loss
+        self.derivative_loss_fct = partial(self.loss_fct, derivative=True)
+
+        if self[-1].g is softmax:
+            self.softmax = True
+
+    def fit(self, x, y, epochs=1, batch_size=128, validation_data=None, gradients_to_check_each_epoch=None, verbose=True):
+
+        ytrain_labels = np.argmax(y, axis=0)
+        Ntrain = x.shape[-1]
+
+        if validation_data:
+            xtest, ytest = validation_data
+            assert xtest.shape[-1] == ytest.shape[-1]
+            ytest_labels = np.argmax(ytest, axis=0)
+            Ntest = xtest.shape[-1]
+        else:
+            val_printout = ''
+
+        if not gradients_to_check_each_epoch:
+            grad_printout = ''
+
+        for epoch in range(1, epochs):
+
+            losses = []
+            self.lr *= 0.993
+
+            minibatches = misc.minibatches(x, y, batch_size=1000)
+            for m, minibatch in enumerate(minibatches):
+
+                self.train_on_batch(*minibatch)
+
+                losses.append(self.get_loss(*minibatch))
+
+                # important: do gradient checking before weights are changed!
+                if gradients_to_check_each_epoch and m == 1:
+                    goodness = self.gradient_checks(*minibatch, eps=10**(-6), checks=3)
+                    grad_printout = f'gradcheck: {goodness:.3e}'
+
+                self.update_weights()
+
+
+            a_train = self(x)
+            ytrain_pred = np.argmax(a_train, axis=0)
+            train_correct = np.sum(ytrain_pred == ytrain_labels)
+            loss = np.mean(losses)
+
+            if validation_data:
+                a_test = self(xtest)
+                ytest_pred = np.argmax(a_test, axis=0)
+                test_correct = np.sum(ytest_pred == ytest_labels)
+                val_loss = self.get_loss(xtest, ytest)
+
+                val_printout = f'{val_loss=:.3f}, test:{test_correct}/{Ntest}'
+
+            print(f'{epoch=}, {loss=:.3f}, train: {train_correct}/{Ntrain}, {val_printout}, {grad_printout}')
+
+            self.epoch += 1
+
 
     def forward_step(self, a):
-
-        self[0].a = a
         if self.verbose: print('START FORWARD STEP')
+        self[0].a = a
         for layer in self:
             a = layer(a)
-            if self.verbose: print(f'{a.shape=}')
-            if self.verbose: print(layer)
         return a
 
     def train_on_batch(self, x, y):
         self.forward_step(x)
         self.backpropagation(x, y)
 
-    def backpropagation(self, x, y):
-        if self.verbose: print('START BACKPROP')
 
-        #first do last layer, then the rest
-        y_pred = self[-1].a
-        back_err = self.derivative_lossfunction(y_pred, y)
+    def backpropagation(self, x, y, verbose=True):
 
-        # the rest of the network
-        for l in range(len(self)-1, 0, -1):
+        error_prev = self._backprop_last_layer(x, y)
 
-            if self.verbose: print('\n' + 30*'*')
-            if self.verbose: print(f'in backprop layer {l=}, using {back_err.shape=}')
+        for l in range(len(self)-2, 0, -1):
 
-            prev_layer = self[l-1]
-            layer = self[l]
+            a_next = self[l - 1].a
+            w_prev = self[l + 1].w
 
-            prev_a = prev_layer.a
-            a = layer.a
+            error_prev = self[l].backward_step(a_next, w_prev, error_prev)
 
-            if self.verbose: print('\nget_error')
-            layer.get_error(back_err)
 
-            if self.verbose: print('\nget_grads')
-            layer.get_grads(prev_a)
+    def _backprop_last_layer(self, x, y):
+        '''
+        calculates the error for the last layer.
+        It is a little bit special as it involves
+        the cost function, so do it in its own function.
+        '''
 
-            if self.verbose: print('\nupdate_weights')
-            layer.update_weights(self.lr)
+        derivative_loss = self.derivative_loss_fct(
+            ypred=self[-1].a,
+            ytrue=y,
+            average_examples=False
+        )
 
-            back_err = layer.backward()
-            #print '\terror shape', layer.error.shape
+        derivative_layer = self[-1].g(
+            z=self[-1].z,
+            derivative=True
+        )
 
-        #self.optimizer.update()
+        if self[-1].g is softmax:
+            deltaL = np.einsum('in,jin->jn', derivative_loss, derivative_layer)
+        else:
+            deltaL = derivative_layer * derivative_loss
 
-    def get_loss(self, x, ytrue):
-        ypred = self.predict(x)
-        loss = self.lossfunction(ypred, ytrue)
+        batch_size = x.shape[-1]
+        self[-1].dw = 1 / batch_size * np.dot(deltaL, self[-2].a.T)
+        self[-1].db = 1 / batch_size * np.sum(deltaL, axis=1, keepdims=True)
+        return deltaL
+
+
+    def update_weights(self):
+        for layer in self[1:]:
+            layer.w -= self.lr * layer.dw
+            layer.b -= self.lr * layer.db
+
+
+    def get_loss(self, x, ytrue, average_examples=True):
+        ypred = self(x)
+        loss = self.loss_fct(ypred, ytrue, average_examples=average_examples)
         return loss
 
-    def predict(self, x):
-        return self.forward_step(x)
 
-    def gradient_check(self, x, ytrue, check_layer, grad_check_info, eps=10**(-7), random_weight = True):
+    def predict(self, x):
+        return self(x)
+
+
+    def complete_gradient_check(self, x, y, eps=10**(-6)):
+        self.grads_ = []
+        for layer in self[1:]:
+
+            gradient_manual = np.zeros(layer.w.shape)
+
+            ranges = [range(dim) for dim in layer.w.shape]
+            for idx in itertools.product(*ranges):
+
+                gradient = self.gradient_check(
+                    x=x,
+                    ytrue=y,
+                    eps=eps,
+                    layer_id=layer.layer_id,
+                    weight_idx=idx
+                )
+
+                gradient_manual[idx] = gradient
+
+            numerator = np.linalg.norm(gradient_manual - layer.dw)
+            denominator = np.linalg.norm(gradient_manual) +  np.linalg.norm(layer.dw)
+
+            goodness = numerator / denominator
+            self.n= numerator
+            self.d=denominator
+            self.grads_.append(gradient_manual)
+            print(f'backprop err layer {layer.layer_id}: {goodness=}')
+
+
+    def gradient_checks(self, x, ytrue, checks=15, eps=10**(-6)):
+        '''
+        Carries out several gradient checks in random places at once
+        '''
+
+        grads = np.zeros(checks)
+        grads_backprop = np.zeros(checks)
+
+        for check in range(checks):
+
+            layer_id = np.random.randint(1, len(self))
+            shape = self[layer_id].w.shape
+            weight_idx = tuple(np.random.choice(dim) for dim in shape)
+            gradient = self.gradient_check(
+                x=x,
+                ytrue=ytrue,
+                eps=eps,
+                layer_id=layer_id,
+                weight_idx=weight_idx
+            )
+
+            grads[check] = gradient
+            grads_backprop[check] = self[layer_id].dw[weight_idx]
+
+        n = np.linalg.norm
+        goodness = n(grads - grads_backprop)/(n(grads) + n(grads_backprop))
+
+        return goodness
+
+
+    def gradient_check(self, x, ytrue, eps, layer_id, weight_idx):
         ''' to test the backprop algorithm, we also manually check the gradient
             for one randomly chosen weight/bias
             do this by using df(x)/dx = (f(x+eps) - f(x-eps)/2/eps'''
-        take_weights = True
-        grad_manual = 0
-        check_k, check_j, check_c_prev, check_c = grad_check_info
-        #a = layer.forward(a)
-        for multiplier in [+1, -1]:
-            tinychange = multiplier * eps
-            a = x
-            for l, layer in list(self.layers.items())[1:]:
-                if l == check_layer:
-                    a = layer.forward(a, gradient_check=True, grad_check_info = grad_check_info + (tinychange,))
-                else:
-                    a = layer.forward(a)
 
-            cost = multiplier * self.get_loss(x, ytrue)
-            grad_manual += cost
+        cost = 0
+        w_original = self[layer_id].w[weight_idx]
 
-        grad_manual /= (2*eps)
+        for sign in [+1, -1]:
 
-        if take_weights:
-            grad_backprop = self.layers[check_layer].dw[check_k, check_j, check_c_prev, check_c]
+            self[layer_id].w[weight_idx] = w_original + sign * eps # change weight
+            cost += sign * self.get_loss(x, ytrue, average_examples=True)
 
-        #if self.verbose: print('in gradcheck:', grad_manual, grad_backprop)
-        if grad_manual == 0:
-            if grad_backprop == 0:
-                ratio = 1
-            else:
-                ratio = np.inf
-        else:
-            ratio =  grad_backprop/grad_manual
-            if abs(ratio-1) > 0.001:
-                if take_weights:
-                    parameter = 'w[%i][%i,%i,%i,%i]' % (check_layer, check_k, check_j, check_c_prev, check_c)
-                if self.verbose: print('ratio backprop/manual=%.5f. cause: %s' % (ratio, parameter))
-        return ratio, grad_manual, grad_backprop
+        self[layer_id].w[weight_idx] = w_original  # restore weight
 
 
-    def save_model(self, name):
-        #h5 file
-        pass
+        gradient_manual = cost / (2*eps)
+        return gradient_manual

src/network_test.py
@@ -1,19 +1,22 @@
 '''
 Trains a neural network with mnist data
 '''
+from tqdm import tqdm
 
 import numpy as np
 import sys
 import os
-np.warnings.filterwarnings('ignore')
-np.random.seed(1)
+#np.warnings.filterwarnings('ignore')
+np.random.seed(2)
+np.set_printoptions(precision=3, linewidth=180)
+np.seterr(all='raise')
 
 from network import Network
 from dense import Dense
 from conv2d import Conv2D
 from flatten import Flatten
 from activations import relu, sigmoid, linear, tanh, softmax, lrelu
-from loss_functions import mse, cross_entropy
+from loss_functions import mse, crossentropy
 import loss_functions
 import misc
 import mnist
@@ -25,10 +28,22 @@ if __name__ == '__main__':
     except IndexError:
         epochs = 2
 
-    xtrain, xtest, ytrain, ytest = mnist.load_mnist(os.environ['HOME'] + '/python/neuralnet/data')
+    datapath = os.environ['HOME'] + '/python/neuralnet/data'
+    xtrain, xtest, ytrain, ytest = mnist.load_mnist(datapath)
+
+    ytrain_labels = np.argmax(ytrain, axis=0)
+    ytest_labels = np.argmax(ytest, axis=0)
+
+
+    xtrain /= 255.0
+    xtest /= 255.0
 
     xtrain.resize((28**2, 60000))
     xtest.resize((28**2, 10000))
+
+    Ntrain=60000
+    Ntest=10000
+
 
     model = Network(verbose=False)
 
@@ -38,14 +53,18 @@ if __name__ == '__main__':
 
     kernel_init = 'glorot_uniform'
     kernel_init= 'normal'
-    act = relu
 
-    depth = 30
+    depth = 10
+    #model.add(Dense(input_dim, output_dim, relu, kernel_init))
     model.add(Dense(input_dim, depth, relu, kernel_init))
     #model.add(Dense(depth, depth, relu, kernel_init))
     #model.add(Dense(depth, depth, relu, kernel_init))
     model.add(Dense(depth, output_dim, softmax, kernel_init))
-    model.compile(loss = mse, lr = 0.001)
+
+    model.compile(loss = crossentropy, lr = 1*10**(-1))
+
+    loss = model.get_loss(xtrain, ytrain, average_examples=True)
+    print(f'basic loss test: {loss}')
 
     '''
 
@@ -64,29 +83,48 @@ if __name__ == '__main__':
     '''
     l = model.layers
 
+    layer = 1
+    idx = (2,3)
+    goodness = None
+
     printeach = 1
     for epoch in range(1, epochs):
-        model.lr *= 0.995
-        minibatches = misc.minibatches(xtrain, ytrain, batch_size=16)
+        losses = []
+        model.lr *= 0.99
+        minibatches = misc.minibatches(xtrain, ytrain, batch_size=1000)
         for m, minibatch in enumerate(minibatches):
-            xmini, ymini = minibatch
+            x, y = minibatch
+            #print(y)
 
-            model.train_on_batch(xmini, ymini)
+            model.train_on_batch(x, y)
 
-            loss = model.get_loss(xmini, ymini)
-            print('epoch', epoch, 'minibatch', m, 'loss',loss)
+            loss = model.get_loss(x, y)
+            losses.append(loss)
+            #print(f'{epoch=}, {m=}, {loss=:4f}')
+            #print(f'{model[layer].w[idx]=:.3f}')
+            if m == 1:
+                model.complete_gradient_check(x, y, eps=10**(-6))
+                goodness = model.gradient_checks(x=x, ytrue=y, eps=10**(-6), checks=15)
+                sys.exit()
+                pass
 
-            #ratio, grad_manual, grad_backprop = model.gradient_check(xmini, ymini, 1, (0,0,0,0))
+            model.update_weights()
+            #model.complete_gradient_check(x, y, eps=10**(-6))
+            #sys.exit()
 
-            #print('gradient checking:', ratio, 'manual calculation', grad_manual, 'backprop', grad_backprop)
+        loss = np.mean(loss)
 
+        a_train = model(xtrain)
+        a_test = model(xtest)
+
+        ytrain_pred = np.argmax(a_train, axis=0)
+        ytest_pred = np.argmax(a_test, axis=0)
+
+        train_correct = np.sum(ytrain_pred == ytrain_labels)
+        test_correct = np.sum(ytest_pred == ytest_labels)
+
+        val_loss = model.get_loss(xtest, ytest)
+        print(f'{epoch=}, {loss=:.3f}, {val_loss=:.3f}, train: {train_correct}/{Ntrain}, test: {test_correct}/{Ntest}, gradcheck: {goodness}')
         model.epoch += 1
 
 
-    pred = model.predict(xtrain)
-    print(pred)
-    print(ytrain)
-
-    ratio, meanerr = loss_functions.mae(pred, ytrain)
-    print('mae', meanerr)
-    print('ratios', ratio)