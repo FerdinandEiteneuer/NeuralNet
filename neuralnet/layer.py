78838687e55d242779585d0d99f409dae8f358f8
Ferdinand Eiteneuer <ferdinand.eiteneuer@rwth-aachen.de>
2021-02-04 22:19:59 +0100

    clean up layer

--- neuralnet/layer.py
+++ neuralnet/layer.py
@@ -1,14 +1,6 @@
 import itertools
 
-class MetaLayer(type):
-
-    def __init__(cls, name, bases, attrs):
-        super().__init__(name, bases, attrs)
-        cls._ids = itertools.count(0)  # each new layer object gets a layer id
-
-
 class Layer():
-#class Layer(metaclass=MetaLayer):
     ''' Base Layer'''
 
     _ids = itertools.count(0)
