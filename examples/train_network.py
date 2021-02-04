1a79f25500fa2b99577941ff6afa0134c69dad3c
Ferdinand Eiteneuer <ferdinand.eiteneuer@rwth-aachen.de>
2021-02-04 20:51:46 +0100

    change minor things

--- examples/train_network.py
+++ examples/train_network.py
@@ -14,42 +14,37 @@ from neuralnet.optimizers import SGD, Nadam
 
 from neuralnet.data import load_mnist
 
+np.random.seed(123)  # reproducibility
+
 if __name__ == '__main__':
 
-    np.random.seed(123)  # reproducibility
-    ########
-    # DATA #
-    ########
     xtrain, xtest, ytrain, ytest = load_mnist.load()
 
     xtrain = np.resize(xtrain, (28**2, 60000))
     xtest = np.resize(xtest, (28**2, 10000))
 
-    ###########
-    # NETWORK #
-    ###########
-
     input_dim = xtrain.shape[0]
     output_dim = ytrain.shape[0]
 
     kernel_init= 'normal'
-    depth = 200
+    depth = 400
 
     model = Sequential()
 
     model.add(Dense(input_dim, depth, relu, kernel_init))
     model.add(Dense(depth, depth, relu, kernel_init))
+    model.add(Dense(depth, depth, relu, kernel_init))
     model.add(Dense(depth, output_dim, softmax, kernel_init))
 
 
-    #sgd = SGD(learning_rate=10**(-1), bias_correction=True, momentum=0.9)
-    nadam = Nadam(learning_rate=10**(-3), beta_1=0.9, beta_2=0.999, eps=10**(-8))
+    sgd = SGD(learning_rate=2*10**(-1), bias_correction=True, momentum=0.9)
+    nadam = Nadam(learning_rate=0.7*10**(-3), beta_1=0.9, beta_2=0.999, eps=10**(-8))
 
     model.compile(loss = crossentropy, optimizer=nadam)
     print(model)
 
     loss = model.get_loss(xtrain, ytrain, average_examples=True)
-    print(f'Sanity check: Initial{loss=:.5f}')
+    print(f'Sanity check: intial {loss=:.5f}')
 
     model.fit(
         x=xtrain,
